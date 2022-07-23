8ceb9b86f994ead4a4956c63b5eb327d8f4d5e98
Simon Li <orpheus+devel@gmail.com>
2022-07-23 19:57:46 +0100

    Unknown traitlets types: object

--- tools/traitlets2schema.py
+++ tools/traitlets2schema.py
@@ -1,40 +1,51 @@
+#!/usr/bin/env python
+import json
 from argparse import ArgumentParser
 from importlib import import_module
-from inspect import getmembers
-import json
-import sys
-from traitlets.traitlets import Any, Bool, Callable, Dict, Float, Int, List, Set, Unicode, Union
+
 import jupyterhub.traitlets
+from traitlets.traitlets import (
+    Any,
+    Bool,
+    Bytes,
+    Callable,
+    Dict,
+    Enum,
+    Float,
+    Int,
+    List,
+    Set,
+    Unicode,
+    Union,
+)
 
 
 def lookup_schema_type(t):
+    UNKNOWN_TYPE = "object"
     traitlets_schema_map = {
         Any: "object",
         Bool: "bool",
-        Callable: None,
+        Bytes: "string",
+        Callable: UNKNOWN_TYPE,
         Dict: "object",
+        Enum: UNKNOWN_TYPE,
         Float: "number",
         Int: "number",
         List: "array",
         Set: "array",
         Unicode: "string",
         Union: None,
-
         jupyterhub.traitlets.ByteSpecification: "string",
-        jupyterhub.traitlets.Callable: None,
+        jupyterhub.traitlets.Callable: UNKNOWN_TYPE,
         jupyterhub.traitlets.Command: "string",
+        jupyterhub.traitlets.EntryPointType: UNKNOWN_TYPE,
+        jupyterhub.traitlets.URLPrefix: "string",
     }
     return traitlets_schema_map[t]
 
 
 # https://json-schema.org/understanding-json-schema/reference/type.html
 def get_schema_types(traitlet):
-    # ignore_type_strs = [f"<class '{t}'>" for t in (
-    #     "traitlets.traitlets.Callable",
-    #     "jupyterhub.traitlets.ByteSpecification",
-    #     "jupyterhub.traitlets.Callable",
-    #     "jupyterhub.traitlets.Command",
-    # )]
     t = lookup_schema_type(traitlet.__class__)
     if t:
         return [t]
@@ -43,28 +54,19 @@ def get_schema_types(traitlet):
         for t in traitlet.trait_types:
             ts.extend(get_schema_types(t))
         return ts
-    return []
+    raise ValueError(f"Unknown traitlet type: {traitlet.__class__}")
 
 
 def get_traitlets_schema(module_name, class_name):
     mod = import_module(module_name)
     cls = getattr(mod, class_name)
 
-    members = getmembers(cls)
-
-    configurables = {}
-    for (k, v) in members:
-        m = getattr(v, "metadata", None)
-        if m and m.get("config"):
-            configurables[k] = v
+    configurables = cls.class_traits(config=True)
 
     schema = {}
 
     for k, v in configurables.items():
         ts = get_schema_types(v)
-        if not ts:
-            print(f"Skipping {k}", file=sys.stderr)
-            continue
         s = {"type": ts}
         if v.allow_none:
             s["type"].append("null")
@@ -76,7 +78,9 @@ def get_traitlets_schema(module_name, class_name):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("module_name", help="Module name containing the class, e.g. 'kubespawner'")
+    parser.add_argument(
+        "module_name", help="Module name containing the class, e.g. 'kubespawner'"
+    )
     parser.add_argument("class_name", help="Class name, e.g. 'KubeSpawner'")
     args = parser.parse_args()
     schema = get_traitlets_schema(args.module_name, args.class_name)
